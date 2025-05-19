1f3a23770649cfd8507e394d64320c15beb6ebd0
Leandro794 <44125429@fi.unju.edu.ar>
2025-05-19 18:24:41 -0300

    fix: Creacion correcta del objeto producto [Leandro Bejarano]

--- tp4/src/App.jsx
+++ tp4/src/App.jsx
@@ -5,13 +5,21 @@ import ProductForm from './components/ProductForm'
 import NavBar from './components/NavBar'
 
 function App() {
+  const [producto, setProducto] = useState({
+    desc:'',
+    price:'',
+    disc:'',
+    stock:'',
+    minprice:''
+  });
+  const [productos, setProductos] = useState([''])
   const [modo, setModo] = useState('list')
   return (
     <div>
         <NavBar modo={[modo, setModo]}></NavBar>
         {(() => {
           switch (modo){
-            case 'new': return <ProductForm></ProductForm>
+            case 'new': return <ProductForm producto={[producto, setProducto]} productos={[productos, setProductos]}/>
             default: return(
               <Header texto={'Gestión de productos'}/>
               )
