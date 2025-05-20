fbad35205305987c309157e866689516a7c33be7
TorresNahuelTobias <tobiastorres123456@gmail.com>
2025-05-20 15:29:40 -0300

    fix: modificacion de la estructura del App.jsx e index.css

--- tp4/src/App.jsx
+++ tp4/src/App.jsx
@@ -4,33 +4,37 @@ import Header from './components/Header'
 import ProductForm from './components/ProductForm'
 import NavBar from './components/NavBar'
 import SearchBar from './components/SearchBar'
-
+import './index.css';
 function App() {
-  const [buscar, setBuscar] = useState (0);
+  const [buscar, setBuscar] = useState(0);
   const [producto, setProducto] = useState({
-    id:'',
-    desc:'',
-    price:'',
-    disc:'',
-    stock:'',
-    minprice:'',
+    id: '',
+    desc: '',
+    price: '',
+    disc: '',
+    stock: '',
+    minprice: ''
   });
-  const [productos, setProductos] = useState([''])
-  const [modo, setModo] = useState('list')
+  const [productos, setProductos] = useState(['']);
+  const [modo, setModo] = useState('list');
+
   return (
-    <div>
-        <NavBar setBuscar={[setBuscar]} modo={[setModo]}></NavBar>
+    <div className="app-container">  
+      <div className="sidebar">
+        <NavBar setBuscar={[setBuscar]} modo={[setModo]} />
+      </div>
+      
+      <div className="main-content">  
         {(() => {
           switch (modo){
             case 'new': return <ProductForm setModo={setModo} producto={[producto, setProducto]} productos={[productos, setProductos]}/>
             case 'buscar': return <SearchBar setModo={setModo} buscar={buscar} productos={productos}/>
-            default: return(
-              <Header texto={'Gestión de productos'}/>
-              )
+            default: return <Header texto={'Gestión de productos'} />;
           }
         })()}
+      </div>
     </div>
   )
 }
 
-export default App
+export default App;
