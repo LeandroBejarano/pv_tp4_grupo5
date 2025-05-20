b89b6e55ffc7af821ffa6ebcaf1ff463a766170e
Leandro794 <44125429@fi.unju.edu.ar>
2025-05-19 21:41:58 -0300

    feat: Creacion del componente SearchBar para buscar productos en base a su ID [Leandro Bejarano]

--- tp4/src/components/SearchBar.jsx
+++ tp4/src/components/SearchBar.jsx
@@ -1,23 +1,29 @@
 import Header from "./Header";
 import Error from "./Error";
+import Section from "./Section";
 function SearchBar (props)
 {
     const buscar = props.buscar;
     const setModo = props.setModo;
     const productos = props.productos;
 
-    const producto = productos.find(p => p.id === parseInt(buscar));
+    const producto = productos.find(p => p.id == Number(buscar));
     return (
     <div>
         <Header texto={'Resultados de la busqueda'}/>
         <button onClick={()=>setModo('list') }>Volver</button>
         {producto ? ( 
-            <div>
+            <Section>    
             <p>Descripción: {producto.desc}</p>
-            </div>
-        ) : (
+            <p>Precio Unitario: {producto.price}</p>
+            <p>Descuento: {producto.disc}%</p>
+            <p>Precio con rebaja: {producto.minprice}</p>
+            <p>Stock: {producto.stock}</p>
+            </Section>
+        )   :   (
         <Error texto={'No se encontró ningún producto'}/>
         )}
+
     </div>
     )
 }
