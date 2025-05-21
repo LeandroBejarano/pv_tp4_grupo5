b8bea68409ff6172688a6232ff169d79f4ca720e
Leandro794 <44125429@fi.unju.edu.ar>
2025-05-20 22:00:19 -0300

    feat: El componente ProductForm ahora puede editar productos existentes [Leandro Bejarano]

--- tp4/src/components/ProductForm.jsx
+++ tp4/src/components/ProductForm.jsx
@@ -1,7 +1,8 @@
 import Header from "./Header";
 import Titulo from "./Titulo";
 function ProductForm (props){
-    const setModo = props.setModo;
+    const [modo,setModo] = props.modo;
+    const [productoActual, setProductoActual] = props.productoActual;
     const [producto, setProducto] = props.producto;
     const [productos, setProductos] = props.productos;
     const inputStyle={
@@ -13,29 +14,59 @@ function ProductForm (props){
     }
 
     const addProducto = () =>{
-        if (producto.desc.trim()==="" || producto.price.trim()==="" ||producto.disc.trim()==="" ||producto.stock.trim()==="")
-            alert ('Error: llene todos los campos y vuelva a intentarlo');
-        else
-        {
-            producto.id=Date.now();
-            producto.minprice=producto.price*(1-producto.disc/100);
-            alert ('Producto agregado');
-            setProductos([...productos,producto]);
-            setProducto({desc:'', disc:'', stock:'', price:''});
+        if (modo === 'new')
+            if (producto.desc.trim()==="" || producto.price.trim()==="" ||producto.disc.trim()==="" ||producto.stock.trim()==="")
+                alert ('Error: llene todos los campos y vuelva a intentarlo');
+            else{
+                producto.id=Date.now();
+                producto.minprice=producto.price*(1-producto.disc/100);
+                alert ('Producto agregado');
+                setProductos([...productos,producto]);
+                setProducto({desc:'', disc:'', stock:'', price:''});
+            }
+        else{
+            if (productoActual.desc.trim()==="" || productoActual.price.trim()==="" || productoActual.disc.trim()==="" || productoActual.stock.trim()==="")
+                alert ('Error: no puede vaciar los campos de un producto existente');
+            else{
+                productoActual.minprice=productoActual.price*(1-productoActual.disc/100);
+                const nuevos = productos.map(p => (p.id === productoActual.id ? productoActual : p));
+                setProductos (nuevos);
+                alert('Producto editado');
+                setModo('list');
+            }
         }
     }
 
+    
     return (
     <div>
-        <Header texto={'Crear producto'}/>
-        <Titulo texto={'No dejar ningun campo vacio'}/>
+        {(() => {
+            switch(modo){
+                case 'new': return(
+                <div>
+                    <Header texto={'Crear producto'}/>
+                    <Titulo texto={'No dejar ningun campo vacio'}/>
+                    <input style={inputStyle} type='text' value={producto.desc} placeholder='Descripcion' onChange={(e)=>setProducto({...producto, desc: e.target.value})}/>
+                    <input style={inputStyle} type='number' value={producto.price} placeholder='Precio unitario' onChange={(e)=>setProducto({...producto, price: e.target.value})}/>
+                    <input style={inputStyle} type='number' value={producto.disc} placeholder='Descuento. Ej: 25' onChange={(e)=>setProducto({...producto, disc: e.target.value})}/>
+                    <input style={inputStyle} type='number' value={producto.stock} placeholder='Stock' onChange={(e)=>setProducto({...producto, stock: e.target.value})}/>
+                    <button onClick={addProducto} >Agregar Producto</button> <br></br>
+                </div>
+                )
+                case 'edit': return(
+                <div>
+                    <Header texto={'Editar producto'}/>
+                    <Titulo texto={'No dejar ningun campo vacio'}/>
+                    <input style={inputStyle} type='text' value={productoActual.desc} placeholder='Descripcion' onChange={(e)=> setProductoActual({...productoActual, desc: e.target.value })}/>
+                    <input style={inputStyle} type='number' value={productoActual.price} placeholder='Precio unitario' onChange={(e)=> setProductoActual({...productoActual, price: e.target.value })}/>
+                    <input style={inputStyle} type='number' value={productoActual.disc} placeholder='Descuento' onChange={(e)=> setProductoActual({...productoActual, disc: e.target.value })}/>
+                    <input style={inputStyle} type='number' value={productoActual.stock} placeholder='Stock' onChange={(e)=> setProductoActual({...productoActual, stock: e.target.value })}/>
+                    <button onClick={addProducto} >Guardar Cambios</button>
+                </div>
+                )
+            }
+        })()}
 
-        <input style={inputStyle} type='text' value={producto.desc} placeholder='Descripcion' onChange={(e)=>setProducto({...producto, desc: e.target.value})}/>
-        <input style={inputStyle} type='number' value={producto.price} placeholder='Precio unitario' onChange={(e)=>setProducto({...producto, price: e.target.value})}/>
-        <input style={inputStyle} type='number' value={producto.disc} placeholder='Descuento. Ej: 25' onChange={(e)=>setProducto({...producto, disc: e.target.value})}/>
-        <input style={inputStyle} type='number' value={producto.stock} placeholder='Stock' onChange={(e)=>setProducto({...producto, stock: e.target.value})}/>
-        <h1 ></h1>
-        <button onClick={addProducto} >Agregar Producto</button> <br></br>
         <button onClick={()=>setModo('list')}>Volver</button>
     </div>
     )
